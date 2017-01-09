d7201c1eab8a9443c9b9ea89546acf67d30ae528
martinmravec <martinmravec4@gmail.com>
2017-01-09 06:22:20 +0100

    full register and login + first step for NV

--- authentification.py
+++ authentification.py
@@ -4,19 +4,40 @@ from flask_login import LoginManager
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import URLSafeTimedSerializer
+from flask_mail import Message, Mail
 from userDB import *
+from functools import wraps
+import os
 
-engine = create_engine('mysql+mysqlconnector://root:@localhost/blog', echo=True)
+engine = create_engine('mysql+mysqlconnector://root:@localhost/test', echo=True)
 
 app = Flask(__name__)
-app.debug = True
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY='7d441f27d441f27567d441f2b6176a',
+    SECURITY_PASSWORD_SALT = 'my_precious_two',
+
+    # mail settings
+    MAIL_SERVER = 'smtp.googlemail.com',
+    MAIL_PORT = 465,
+    MAIL_USE_TLS = False,
+    MAIL_USE_SSL = True,
+
+
+    # mail accounts
+    MAIL_USERNAME = 'martinmravec4@gmail.com',
+    MAIL_PASSWORD = 'asdfghjkl123qwert'
+)
 
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = 'login'
+
+mail = Mail(app)
 
 
 @app.route('/')
@@ -29,13 +50,52 @@ def index():
 def register():
     if request.method == 'GET':
         return render_template('register.html')
-    user = User(request.form['username'], request.form['password'], request.form['email'])
+    user = User(request.form['username'], request.form['password'], request.form['email'], confirmed=False)
     Session = sessionmaker(bind=engine)
     s = Session()
     s.add(user)
     s.commit()
-    flash('User successfully registred')
-    return redirect(url_for('login'))
+    token = generate_confirmation_token(user.email)
+    confirm_url = url_for('confirm_email', token=token, _external=True)
+    html = render_template('activate.html', confirm_url=confirm_url)
+    subject = "Please confirm your email."
+    """
+        Not work in tssk-proxy
+    """
+    """try:
+        send_email(user.email, subject, html)
+        login_user(user)
+
+        flash('User successfully registred. A confirmation email has been sent via email.', 'success')
+        return redirect(url_for('login'))
+    except:
+    """
+    login_user(user)
+    """
+    return redirect(url_for("unconfirmed"))
+    """
+    return html
+
+
+@app.route('/confirm/<token>')
+@login_required
+def confirm_email(token):
+    try:
+        email = confirm_token(token)
+    except:
+        flash('This confirmation link is invalid or has expired.', 'danger')
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    user = s.query(User).filter_by(email=email).first()
+    if user.confirmed:
+        flash('Account allredy confirmed. Please login.', 'success')
+    else:
+        user.confirmed = True
+        user.confirmed_on = datetime.utcnow()
+        s.add(user)
+        s.commit()
+        flash('You have confirmed your account. Thanks!', 'success')
+    return redirect(url_for('profile'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -52,7 +112,10 @@ def login():
     Session = sessionmaker(bind=engine)
     s = Session()
     registered_user = s.query(User).filter_by(username=username).first()
-
+    confirmed_user = s.query(User).filter_by(confirmed=True).first()
+    if confirmed_user is None:
+        flash('Your account has not been activated yet.', 'error')
+        return redirect(url_for('login'))
     if registered_user is None:
         flash('Username or Password is invalid', 'error')
         return redirect(url_for('login'))
@@ -62,6 +125,54 @@ def login():
     login_user(registered_user, remember=remember_me)
     flash('Logged in successfully')
     return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/unconfirmed')
+@login_required
+def unconfirmed():
+    if current_user.confirmed:
+        return redirect('login')
+    flash('Please confirm your account!', 'warning')
+    return render_template('unconfirmed.html')
+
+
+def check_confirmed(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if current_user.confirmed is False:
+            flash('Please confirm your account!','warning')
+            return redirect(url_for('unconfirmed'))
+        return func(*args, **kwargs)
+
+    return decorated_function
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/navigation', methods=['GET', 'POST'])
+@login_required
+@check_confirmed
+def navigation():
+    return render_template('navigation.html')
+
+
+@app.route('/resend')
+@login_required
+def resend_confirmation():
+    token = generate_confirmation_token(current_user.email)
+    confirm_url = url_for('confirm_email', token=token, _external=True)
+    html = render_template('activate.html', confirm_url = confirm_url)
+    subject = "Please confirm your email"
+    """
+    send_email(current_user.email, subject, html)
+    """
+    flash('A new confirmation email has been sent.', 'success')
+    return html
 
 
 @app.route('/logout')
@@ -82,6 +193,34 @@ def before_request():
     g.user = current_user
     if current_user is None:
         return render_template('login.html')
+
+
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
+
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(
+            token,
+            salt=app.config['SECURITY_PASSWORD_SALT'],
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
+
+
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=app.config['MAIL_USERNAME']
+    )
+    mail.send(msg)
 
 
 if __name__ == '__main__':
--- userDB.py
+++ userDB.py
@@ -1,35 +1,55 @@
 from datetime import datetime
 from sqlalchemy import *
 from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Integer, DATETIME, VARCHAR
+from sqlalchemy import Column, Integer, DATETIME, VARCHAR, BOOLEAN
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-engine = create_engine('mysql+mysqlconnector://root:@localhost/blog', echo=True)
+"""engine = create_engine('mysql+mysqlconnector://root:@localhost/blog', echo=True)"""
+engine = create_engine('mysql+mysqlconnector://root:@localhost/test', echo=True)
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = "users"
 
+    """
     id = Column('used_id', Integer, primary_key=true)
     username = Column('username', VARCHAR(20), unique=True, index=True)
     password = Column('password', VARCHAR(250))
-    email = Column('email', VARCHAR(50), unique=True,index=True)
+    email = Column('email', VARCHAR(50), unique=True, index=True)
     registred_on = Column('registred_on', DATETIME)
 
-    def __init__(self, username, password, email):
+
+    def __init__(self, username, password,  email):
         self.username = username
         self.set_password(password)
         self.email = email
         self.registred_on = datetime.utcnow()
+    """
 
-    def set_password(self , password):
+    id = Column('used_id', Integer, primary_key=true)
+    username = Column('username', VARCHAR(20), unique=True, index=True)
+    password = Column('password', VARCHAR(250))
+    email = Column('email', VARCHAR(50), unique=True, index=True)
+    registred_on = Column('registred_on', DATETIME)
+    confirmed = Column('confirmed', BOOLEAN, nullable=False, default=False)
+    confirmed_on = Column('confirmed_on', DATETIME, nullable=True)
+
+    def __init__(self, username, password, email, confirmed, confirmed_on=None):
+        self.username = username
+        self.set_password(password)
+        self.email = email
+        self.registred_on = datetime.utcnow()
+        self.confirmed = confirmed
+        self.confirmed_on = confirmed_on
+
+    def set_password(self, password):
         self.password = generate_password_hash(password)
 
-    def check_password(self , password):
+    def check_password(self, password):
         return check_password_hash(self.password, password)
 
     def is_authenticated(self):
